ea22b7f49355c6efca70c6702f01e99e591f8cf7
MahZ21 <Mahletz2711@gmail.com>
2022-09-13 16:05:40 +0300

    Classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+
+class Square:
+    """
+    This is an empty class that defines the Square
+    """
+    pass
+    
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+
+class Square:
+    """
+    This is a class that defines the Square with size
+    """
+    def __init__(self, size):
+        self.__size = size
+        
