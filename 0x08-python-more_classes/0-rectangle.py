d8283d3f9e3e34d26de63bc76c1134e827b2fdc1
MahZ21 <Mahletz2711@gmail.com>
2022-09-21 13:22:14 +0300

    phyton more classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-Defines an empty class Rectangle
-"""
+"""class Rectangle"""
 
 
 class Rectangle:
-    """Empty representation of a rectangle"""
-    pass
+    """defines a rectangle"""
     
