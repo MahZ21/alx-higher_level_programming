211589c38aa62d58387bb9f1ee37fd8e61531eca
MahZ21 <101326355+MahZ21@users.noreply.github.com>
2022-12-01 03:04:30 +0300

    updated 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const num = parseInt(process.argv[2], 10);
+if (!num) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + Number(process.argv[2]));
 }
