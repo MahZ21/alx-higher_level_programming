ebab49b46c891073f4f70c317ad30763d762d6c1
MahZ21 <101326355+MahZ21@users.noreply.github.com>
2022-12-20 13:47:07 +0300

    commit 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
-if __name__ == '__main__':
+# Task 8. Search API
+if __name__ == "__main__":
+    import sys
     import requests
-    from sys import argv
-    if len(argv) == 2:
-        q = argv[1]
+    the_url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        the_letter = {"q": sys.argv[1]}
     else:
-        q = ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        the_letter = {'q': ""}
+    my_req = requests.post(the_url, the_letter)
     try:
-        r_dict = r.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
+        the_resp = my_req.json()
+        if not the_resp:
             print("No result")
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+            print("[{}] {}".format(the_resp.get('id'), the_resp.get('name')))
     except:
         print("Not a valid JSON")
