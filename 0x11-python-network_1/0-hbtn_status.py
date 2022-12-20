413dcb23348ec4c215eb9b5b9a8bc9b6d497e70a
MahZ21 <101326355+MahZ21@users.noreply.github.com>
2022-12-20 13:03:43 +0300

    new 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+# Task 0. What's my status? #0
 import urllib.request
-
-
-def fetcher():
-    """fetcher"""
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-if __name__ == "__main__":
-    fetcher()
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
